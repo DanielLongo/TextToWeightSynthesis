6477f4b5ce4cb308b80f68c0cbdb79157fef611b
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-12-16 21:57:06 -0800

    updates

--- utils.py
+++ utils.py
@@ -6,3 +6,10 @@ def reconstruction_loss(input_text, reconstructed_text):
 	loss = torch.nn.functional.binary_cross_entropy(reconstructed_text, input_text)
 	out = loss
 	return out
+
+def apply_loss(score_true, score_false):
+	#TODO: make better loss op!
+	score_true = score_true.float().detach()
+	score_false = score_false.float().detach()
+	loss = score_false - score_true
+	return loss
